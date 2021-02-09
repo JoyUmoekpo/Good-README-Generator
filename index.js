af45d5a8199da62b600cd73685d3acdbcb3b45ee
Joy <joyumoekpo1@gmail.com>
2021-02-09 15:40:49 -0600

    added dependencies and functions to index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-var fs = require('fs');
-var axios = require('axios');
-var inquirer = require('inquirer');
-markDown = require('generateMarkdown.js');
+const fs = require('fs');
+const axios = require('axios');
+const inquirer = require('inquirer');
+const markDown = require('generateMarkdown.js');
+
+const questions = [
+    {
+        
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
