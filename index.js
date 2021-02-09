b3d1963487034d4d401be8637bc5c74894702c60
Joy <joyumoekpo1@gmail.com>
2021-02-09 16:06:21 -0600

    edited questions

--- index.js
+++ index.js
@@ -5,7 +5,41 @@ const markDown = require('generateMarkdown.js');
 
 const questions = [
     {
-        
+        type: "input",
+        message: "Please enter your Github username.",
+        name: "userName"
+    },
+    {
+        type: "input",
+        message: "Please enter your email address.",
+        name: "userEmail",
+    },
+    {
+        type: "input",
+        message: "Please enter your project title.",
+        name: "projectTitle",
+    },
+    {
+        type: "input",
+        message: "Please enter a description for this project.",
+        name: "projectDescription",
+    },
+    {
+        type: "input",
+        message: "What command should be run to start tests?",
+        default: "`npm test`",
+        name: "projectTest",
+    },
+    {
+        type: "input",
+        message: "Any additional information about the project?",
+        name: "projectInfo",
+    },
+    {
+        type: "input",
+        message: "Please input an appropriate license type for this project",
+        default: "MIT",
+        name: "projectLicense",
     }
 ];
 
